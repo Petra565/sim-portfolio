c89318a4ea19956d9be3a5d00dc229c5ad068e70
Petra565 <sim.petra01@gmail.com>
2025-07-03 14:39:49 +0200

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,28 @@
+function About() {
+
+    return (
+        <>
+            <section className="about">
+
+                <h4 className="headings my-4">O mne</h4>
+
+                <p>
+                    Som začínajúca frontend developerka. Na cestu programovania som sa síce vydala len nedávno, no verím, že po nej budem kráčať dlho a s čoraz väčšími vedomosťami. Svet technológií ma vždy zaujímal, no až doteraz som nenašla odvahu doň vstúpiť.
+                    Pracovala som v oblasti retailu, kde som sa dostala na vedúcu pozíciu a využila svoje organizačné schopnosti, ako súčasť manažmentu prevádzky. Vždy som bola tímový hráč s veľkým zmyslom pre zodpovednosť a pracovala som na 120 %. Postupne som si však uvedomovala, že práca v retaili ma nenapĺňa, a cítila som potrebu pôsobiť v oblasti, kde uvidím výsledky svojej práce a zároveň budem môcť využiť svoju kreatívnu stránku.
+                </p>
+
+                <p>
+                    Prišiel zlomový bod a ja som sa rozhodla dať výpoveď, aby som sa mohla naplno venovať štúdiu programovania, o ktorom som roky premýšľala. Vďaka predplatnému na Skillmea.sk a s pomocou seniorného frontend developera som sa začala učiť základy programovania a postupne vytvárala vlastné malé projekty. Uvedomujem si, že som ešte len na začiatku, no som pripravená sa ďalej vzdelávať a s radosťou rozširovať svoje vedomosti.
+                </p>
+
+                <p>
+                    Budem vďačná, ak dostanem príležitosť uplatniť sa v tomto odbore.
+                </p>
+
+            </section>
+
+        </>
+    )
+}
+
+export default About
---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,80 @@
+﻿import CarouselComponent from './CarouselComponent'
+
+//Bootstrap
+import Image from 'react-bootstrap/Image';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+function Portfolio() {
+
+    const accommodationImages = [
+        { image: "/images/AccommodationProject01.png" },
+        { image: "/images/AccommodationProject02.png" },
+        { image: "/images/AccommodationProject03.png" },
+        { image: "/images/AccommodationProject04.png" },
+    ]
+     const interierImages = [
+        { image: "/images/Interier01.png" },
+        { image: "/images/Interier02.png" },
+        { image: "/images/Interier03.png" },
+    ]
+
+    return (
+        <>
+            <section className="portfolio">
+
+                <h4 className="headings my-4">Portfólio</h4>
+
+                <Row className="justify-content-md-center text-start">
+                    <h5>Informačný portál určený pre poskytovateľov ubytovania</h5>
+                    <p>Stránka, kde si poskytovatelia ubytovania vedia efektívne evidovať všetky informácie o objednávkach. Portál umožňuje vytvárať objednávky a cenník, upravovať ich a vymazať, sprostredkováva check-in formulár pre hostí, zobrazuje objednávky v kalendári a v sumarizuje celkové výsledky v grafoch.   </p>
+                </Row>
+
+                <Row className="mx-0 mb-4">
+                    <Button className=" buttonBgColor w-full" as="a" href="https://github.com/Petra565/accomodation.git" target="_blank" rel="noopener noreferrer"><strong>Odkaz na GitHub repozitár</strong></Button>
+                </Row>
+
+                <CarouselComponent
+                    carouselImages={accommodationImages}
+                />
+
+                <Row className="justify-content-md-center text-start">
+                    <p className="my-3"><strong>Použité technológie:</strong></p>
+                    <ul className="list-inside">
+                        <li>JavaScript, React</li>
+                        <li>Bootstrap</li>
+                    </ul>
+                </Row>
+
+                <Row className="justify-content-md-center text-start">
+                    <h5 className="mt-4">Prezentačná stránka pre malú stolársku výrobu</h5>
+                    <p>Jednoduchá stránka pozostávajúca z úvodnej obrazovky, galérie a kontaktnej stránky.</p>
+                </Row>
+
+                <Row className="mx-0 mb-4">
+                    <Button className=" buttonBgColor w-full" as="a" href="https://github.com/Petra565/InterierWeb.git" target="_blank" rel="noopener noreferrer"><strong>Odkaz na GitHub repozitár</strong></Button>
+                </Row>
+
+                 <Row className="mx-0 mb-4">
+                    <Button variant="secondary" className=" w-full" as="a" href="https://simoncic-interier.sk" target="_blank" rel="noopener noreferrer"><strong>Odkaz na stránku</strong></Button>
+                </Row>
+
+                <CarouselComponent
+                    carouselImages={interierImages}
+                />
+
+                <Row className="justify-content-md-center text-start">
+                    <p className="my-3"><strong>Použité technológie:</strong></p>
+                    <ul className="list-inside">
+                        <li>JavaScript, React</li>
+                        <li>Tailwind</li>
+                    </ul>
+                </Row>
+
+            </section>
+        </>
+    )
+}
+
+export default Portfolio
